732aedbf45f92d3345788813b6e937fae355697f
luqmansen <luqmansen@gmail.com>
2019-10-15 13:54:06 +0700

    Solved All Array Challenge in DS

--- Array Manipulation/main.go
+++ Array Manipulation/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -44,54 +45,54 @@ func arrayManipulation2(n int, queries [][]int) int {
 }
 
 func main() {
-	q := [][]int{{1, 2, 100}, {2, 5, 100}, {3, 4, 100}}
+	//q := [][]int{{1, 2, 100}, {2, 5, 100}, {3, 4, 100}}
 	//q := [][]int{{2, 6, 8}, {3, 5, 7}, {1, 8, 1}, {5, 9, 15}}
-	x := arrayManipulation2(5, q)
-	fmt.Println(x)
-	//reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
-	//
-	////stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	//stdout, err := os.Create("OUTPUT_PATH")
-	//checkError(err)
-	//
-	//defer stdout.Close()
-	//
-	//writer := bufio.NewWriterSize(stdout, 1024 * 1024)
-	//
-	//nm := strings.Split(readLine(reader), " ")
-	//
-	//nTemp, err := strconv.ParseInt(nm[0], 10, 64)
-	//checkError(err)
-	//n := int32(nTemp)
-	//
-	//mTemp, err := strconv.ParseInt(nm[1], 10, 64)
-	//checkError(err)
-	//m := int32(mTemp)
-	//
-	//var queries [][]int32
-	//for i := 0; i < int(m); i++ {
-	//	queriesRowTemp := strings.Split(readLine(reader), " ")
-	//
-	//	var queriesRow []int32
-	//	for _, queriesRowItem := range queriesRowTemp {
-	//		queriesItemTemp, err := strconv.ParseInt(queriesRowItem, 10, 64)
-	//		checkError(err)
-	//		queriesItem := int32(queriesItemTemp)
-	//		queriesRow = append(queriesRow, queriesItem)
-	//	}
-	//
-	//	if len(queriesRow) != int(3) {
-	//		panic("Bad input")
-	//	}
-	//
-	//	queries = append(queries, queriesRow)
-	//}
-	//
-	//result := arrayManipulation(n, queries)
-	//
-	//fmt.Fprintf(writer, "%d\n", result)
-	//
-	//writer.Flush()
+	//x := arrayManipulation2(5, q)
+	//fmt.Println(x)
+	reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+	//stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	stdout, err := os.Create("OUTPUT_PATH")
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+	nm := strings.Split(readLine(reader), " ")
+
+	nTemp, err := strconv.ParseInt(nm[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	mTemp, err := strconv.ParseInt(nm[1], 10, 64)
+	checkError(err)
+	m := int32(mTemp)
+
+	var queries [][]int32
+	for i := 0; i < int(m); i++ {
+		queriesRowTemp := strings.Split(readLine(reader), " ")
+
+		var queriesRow []int32
+		for _, queriesRowItem := range queriesRowTemp {
+			queriesItemTemp, err := strconv.ParseInt(queriesRowItem, 10, 64)
+			checkError(err)
+			queriesItem := int32(queriesItemTemp)
+			queriesRow = append(queriesRow, queriesItem)
+		}
+
+		if len(queriesRow) != int(3) {
+			panic("Bad input")
+		}
+
+		queries = append(queries, queriesRow)
+	}
+
+	result := arrayManipulation(n, queries)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
 }
 
 func readLine(reader *bufio.Reader) string {
