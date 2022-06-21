e1b5c25343b45a6c1a86b7d67bff2315401f1856
Luqman Setyo <38354405+luqmansen@users.noreply.github.com>
2022-06-21 09:43:59 +0700

    Time: 40 ms (65.46%), Space: 14 MB (33.06%) - LeetHub

---
+++ 140-word-break-ii/140-word-break-ii.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        res = self.dfs(s, wordDict)
+
+        if res is None:
+            return []
+        
+        for i in res:
+            if len("".join(i.split(" "))) != len(s):
+                return []
+
+        return res
+
+    def dfs(self, s: str, wordDict: List[str]) -> List[str]:
+        if s == "":
+            return []
+
+        result = None
+        for word in wordDict:
+            if word == s[:len(word)]:
+                remainder = s[len(word):]
+                comb = self.dfs(remainder, wordDict)
+                if comb is None:
+                    continue
+
+                if len(comb) > 0:
+                    for i, v in enumerate(comb):
+                        comb[i] = word + " " + "".join(v)
+                    if result is None:
+                        result = []
+                    result = [*comb, *result]
+                else:
+                    if result is None:
+                        result = []
+                    result = [word, *result]
+
+        return result
+
