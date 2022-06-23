559e6dc79413699e77186c1bd96227d2ad2339c8
Luqman Setyo <38354405+luqmansen@users.noreply.github.com>
2022-06-23 10:21:25 +0700

    Time: 59 ms (20.21%), Space: 13.8 MB (69.31%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,23 @@
+# {[][]}
+# {{}()}([]{})
+
+# stack [ ( ] ) ( ) ( )
+# [ ( ] ) ( ) ( )
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        close = {"}":"{", ")":"(", "]":"["}
+        
+        for i in s:
+            if i in close:
+                if stack and stack[-1] == close[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+                       
+        return len(stack) == 0
+                
+                       
+                
