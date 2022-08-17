8f6762d575859d71965b2608d4fc4eb5cf9e755e
Luqman Setyo <38354405+luqmansen@users.noreply.github.com>
2022-08-17 18:03:14 +0700

    Time: 220 ms (15.23%), Space: 19 MB (33.51%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -3,9 +3,15 @@ class Solution:
         
         an = {} # key word, v [w]
         
+        # time complexity = n * m log m
+        
         for w in strs:
-            s = sorted(w)
-            s = "".join(s)
+            count = [0] * 26
+            
+            for c in w:
+                count[ord(c) - ord("a")] += 1
+            
+            s = tuple(count)
             
             if s in an:
                 an[s].append(w)
@@ -15,7 +21,7 @@ class Solution:
         l = []
         for _, v in an.items():
             l.append(v)
-            
+        
         return l
                 
         
