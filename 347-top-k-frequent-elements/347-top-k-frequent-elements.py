de5c934c50b40a55877a24da13164a900776944c
Luqman Setyo <38354405+luqmansen@users.noreply.github.com>
2022-08-17 19:44:07 +0700

    Time: 130 ms (76.48%), Space: 18.7 MB (48.17%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -2,25 +2,31 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         cnt = {}
         
+        maks= 0
+        
         for n in nums:
             if n in cnt:
                 cnt[n] +=1
             else:
                 cnt[n] = 1
+            
+            maks = max(maks, cnt[n])
         
-        res = []
-        
+        res = [0] * (maks+1)
+
         for key, v in cnt.items():
-            res.append((key,v))
+            if res[v] == 0:
+                res[v] = [key]
+            else:
+                res[v].append(key)
         
-        # print(cnt)
-        # print(res)
-        res = sorted(res, reverse=True, key=lambda x: x[1])
-        # print(res)
+        # O (n log n) solution, because we do sorting 
+        # res = sorted(res, reverse=True, key=lambda x: x[1])
+        # return [i[0] for i in res][:k]
         
-            
-        return [i[0] for i in res][:k]
-            
-            
+        kk = []
+        for i in reversed(res):
+            if i != 0:
+                kk = [*kk, *i]
         
-        
+        return kk[:k] 
