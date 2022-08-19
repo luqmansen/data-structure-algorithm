8326a7ee6fb6f65fd66a809b84e29b7a7ed9df41
Luqman Setyo <38354405+luqmansen@users.noreply.github.com>
2022-08-19 09:51:21 +0700

    Time: 195 ms (16.83%), Space: 14 MB (35.29%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -0,0 +1,63 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        # check row
+        for row in board:
+            items = set()
+            for i in row:
+                if i in items and i != ".":
+                    return False
+                else:
+                    items.add(i)
+        
+        # check col
+        idx = 0
+        for _ in board:
+            items = set()
+            for row in board:
+                i = row[idx]
+                
+                if i in items and i != ".":
+                    return False
+                else:
+                    items.add(i)
+            idx +=1
+            
+        # check box
+        conv = [[] for i in range(9)]
+        pos = 0
+        for i in range(9):
+            
+            if i == 3:
+                pos = 3
+            elif i == 6:
+                pos = 6
+            
+            rowX, rowY = 0, 3
+            for j in range(3):
+                
+                this= board[i][rowX:rowY]
+                # print(pos + j,i, rowX, rowY, this)
+
+                conv[pos + j] = [*conv[pos + j], *this]
+                              
+                if rowY == 9:
+                    break
+
+                rowX, rowY = rowY, rowY + 3
+            
+        for row in conv:
+            items = set()
+            for i in row:
+                if i in items and i != ".":
+                    return False
+                else:
+                    items.add(i)
+                
+        return True
+                
+            
+            
+            
+            
+        
