7e21f96cf1301029577a0a793fcdd91219750746
Luqman Setyo <38354405+luqmansen@users.noreply.github.com>
2022-10-09 20:50:51 +0700

    Time: 9310 ms (7.39%), Space: 17.6 MB (12.74%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -0,0 +1,26 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+                
+        def dfs(idx, memo):
+            if idx in memo:
+                return memo[idx]
+            
+            maks = 0
+            
+            for i in range(idx, len(nums)):
+                if nums[i] > nums[idx] and i > idx:
+                    maks = max (maks, 1 + dfs(i, memo))
+            
+            memo[idx] = maks
+            return maks
+        
+        
+        m = 0
+        memo = {}
+        
+        for i, _ in enumerate(nums):
+            m = max(m, 1 + dfs(i, memo))
+            
+        return m
+            
+        
