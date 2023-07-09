2a46716fcc6be57573762fa152331540f780b2bb
Luqman <38354405+luqmansen@users.noreply.github.com>
2023-07-09 20:54:58 +0700

    Time: 137 ms (16.92%), Space: 21.2 MB (38.23%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,34 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        from collections import defaultdict
+from collections import defaultdict
         
+class Solution:
+    def _calc(self, word) -> list:
+        m = [0] * 26 # at most 26 alphabet english lowercase
+        for c in word:
+            idx = ord(c) - ord('a')
+            m[idx]+=1
+        return m
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        O(n * 26) solution
+        """
+  
+        g = defaultdict(list)
+
+        for s in strs:
+            key = str(self._calc(s))
+            g[key].append(s)
+
+        return g.values()
+
+
+
+    def groupAnagrams_2(self, strs: List[str]) -> List[List[str]]:
+        """
+        O(n * nlog(n)) solution
+        for each input, need to sort the string (n log(n))
+        """
+
         g = defaultdict(list)
 
         for s in strs:
